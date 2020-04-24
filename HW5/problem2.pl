02a8a55f34e47e7e671eec9f579436ed8ac6c729
emilydoran1 <35508852+emilydoran1@users.noreply.github.com>
2020-04-24 13:48:34 -0400

    updating program with cuts

--- HW5/problem2.pl
+++ HW5/problem2.pl
@@ -7,20 +7,23 @@ substitute(Old,New,Term,Term) :-
     atomic(Term),
     Term \= Old.
 
+%% cut is placed before recursive call because after the functors are set you do not want to backtrack
 substitute(Old,New,Term,Term1) :-
     compound(Term),
     functor(Term,F,N),
     functor(Term1,F,N),
+    !,
     substitute(N,Old,New,Term,Term1).
 
-%% a cut is placed after first recursive call and second arg call because the program does not need to backtrack after the term was substituted
+%% cuts are placed before recursive calls because the program does not need to backtrack after the term was substituted
 substitute(N,Old,New,Term,Term1) :-
     N > 0,
     arg(N,Term,Arg),
+    !,
     substitute(Old,New,Arg,Arg1),
     arg(N,Term1,Arg1),
-    !,
     N1 is N-1,
+    !,
     substitute(N1,Old,New,Term,Term1).
 
 substitute(0,Old,New,Term,Term1).
